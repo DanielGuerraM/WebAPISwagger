1aca8f10d64a1efd4f74eecef665f1b50313b351
Daniel Mateo Guerra Montoya <dmguerra@correo.iue.edu.co>
2022-11-18 14:51:26 -0500

    Se crea validaciones a nivel del modelo

--- WebAPIAutores/Entidades/Autor.cs
+++ WebAPIAutores/Entidades/Autor.cs
@@ -3,18 +3,40 @@ using WebAPIAutores.Validaciones;
 
 namespace WebAPIAutores.Entidades
 {
-    public class Autor
+    public class Autor: IValidatableObject
     {
         public int Id { get; set; }
         [Required(ErrorMessage = "El campo {0} es obligatorio")]
-        [PrimeraLetraMayuculaAtribute]
+        //[PrimeraLetraMayuculaAtribute]
         public string Nombres { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
         [PrimeraLetraMayuculaAtribute]
         public string PrimerApellido { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
         [PrimeraLetraMayuculaAtribute]
         public string SegundoApellido { get; set; }
         public string Nacionalidad { get; set; }
         public string FechaNacimiento { get; set; }
         public List<Libro> Libros { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if(!string.IsNullOrEmpty(Nombres))
+            {
+                var primeraLetra = Nombres[0].ToString();
+
+                if (primeraLetra != primeraLetra.ToUpper())
+                {
+                    yield return new ValidationResult("La primera letra debe ser mayuscula",
+                        new string[] {nameof(Nombres) });
+                }
+            }
+
+            if(Nacionalidad == null)
+            {
+                yield return new ValidationResult("El campo nacionalidad no puede ir vacio",
+                    new string[] {nameof(Nacionalidad)} );
+            }
+        }
     }
 }
